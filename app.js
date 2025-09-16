b7736750ad7fc8ab02c28830a848fc3f4bbccd84
MaliSoi <malalasoifer@gmail.com>
2025-09-16 05:08:00 -0300

    feat:se agregó el boton +Agregar Producto

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
-document.addEventListener("DOMContentLoaded", () => {
 //URL del MockApi
-
 const API_URL = "https://68c7410c442c663bd0291346.mockapi.io/api/v1/products/productos";
+
+
+
 
 const $ = id => document.getElementById(id);
 
@@ -22,19 +23,29 @@ const modalTitle = $("modalTitle");
 const closeModal = $("closeModal");
 const cancelModal = $("cancelModal");
 const saveProduct = $("saveProduct");
+const addProductBtn = $("addProductBtn");
 
 const productName = $("productName");
 const productCategory = $("productCategory");
 const productPrice = $("productPrice");
 const productImage = $("productImage");
 
-let productos = []; // lista completa de productos desde la API
+//Modal de eliminación
+const deleteModal = $("deleteModal");
+const closeDeleteModal = $("closeDeleteModal");
+const confirmDelete = $("confirmDelete");
+const cancelDelete = $("cancelDelete");
+const deleteProductName = $("deleteProductName");
+
+let products = []; // lista completa de productos desde la API
 let editId = null; // id del producto que se está editando
+let deleteId = null; //id del producto a eliminar
 
+
+console.log("Inicializando aplicación...");
 console.log("URL de la API:", API_URL);
-console.log("Referencias DOM:", productList, searchInput, categoryFilter);
+console.log("Referencias DOM obtenidas correctamente");
 
-});
+//Funciones de la API (CRUD)
 
-
-
+//Obtener todos los productos desde MockAPI
